fd750934342e731026653aa74b44b615249e3372
Beatrice Bridge <beatricembridge@gmail.com>
2021-10-13 22:36:52 -0700

    Added music and piano button functionality to the app.js file

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const scoreBoxes = [
   ["#awayFive", "#homeFive"],
   ["#awaySix", "#homeSix"],
   ["#awaySeven", "#homeSeven"],
+  ["#awayR", "#homeR"],
 ];
 
 //queries for the six elements on control center
-//row2Col1.innerHTML = 3;
 
 let whoNode = document.querySelector("#who");
 let whoNodeVal = whoNode.value;
@@ -30,12 +30,15 @@ inningNode.addEventListener("change", function (evt) {
 });
 
 let plusRunNode = document.querySelector("#plusButton");
+//increment run buttons:
 
 plusRunNode.addEventListener("click", function (evt) {
-  //add one unless over 99
+  //add one
   let str = scoreBoxes[inningNodeVal - 1][whoNodeVal - 1];
   let scoreToAdd = document.querySelector(str);
   scoreToAdd.innerHTML++;
+  let totalScore = document.querySelector(scoreBoxes[7][whoNodeVal - 1]);
+  totalScore.innerHTML++;
 });
 
 let minusRunNode = document.querySelector("#minusButton");
@@ -46,5 +49,38 @@ minusRunNode.addEventListener("click", function (evt) {
   let scoreToSub = document.querySelector(str);
   if (scoreToSub.innerHTML > 0) {
     scoreToSub.innerHTML--;
+    let totalScore = document.querySelector(scoreBoxes[7][whoNodeVal - 1]);
+    totalScore.innerHTML--;
   }
+});
+//music and picture change:
+let noteNode = document.querySelector("#musButton");
+noteNode.addEventListener("click", function (evt) {
+  let rand2 = Math.floor(Math.random() * 2);
+  let rand3 = Math.floor(Math.random() * 3);
+  if (rand2 === 0) {
+    document.querySelector("#pageImage").src = "images/megaphone.png";
+  }
+  if (rand2 === 1) {
+    document.querySelector("#pageImage").src = "images/ups.png";
+  }
+  if (rand3 === 0) {
+    var audio = new Audio("sounds/buildup.mp3");
+    audio.play();
+  }
+  if (rand3 === 1) {
+    var audio = new Audio("sounds/charge1.mp3");
+    audio.play();
+  }
+  if (rand3 === 2) {
+    var audio = new Audio("sounds/charge2.mp3");
+    audio.play();
+  }
+});
+
+let pianoNode = document.querySelector("#pianoButton");
+pianoNode.addEventListener("click", function (evt) {
+  document.querySelector("#pageImage").src = "images/sdchicken.jpg";
+  var audio = new Audio("sounds/chickendance.mp3");
+  audio.play();
 });
